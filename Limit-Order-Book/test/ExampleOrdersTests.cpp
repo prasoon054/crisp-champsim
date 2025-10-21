60e8da4a794d54fced76842f44463d2085418ab1
Prasoon Kumar <prasoonkumar054@gmail.com>
2025-10-21 13:07:16 +0530

    Added Limit Order Book along with its build binaries
    Signed-off-by: Prasoon Kumar <prasoonkumar054@gmail.com>

---
+++ Limit-Order-Book/test/ExampleOrdersTests.cpp
@@ -0,0 +1,39 @@
+#include "../Limit_Order_Book/Limit.hpp"
+#include "../Limit_Order_Book/Order.hpp"
+#include "../Limit_Order_Book/Book.hpp"
+#include "../Process_Orders/OrderPipeline.hpp"
+#include "../Generate_Orders/GenerateOrders.hpp"
+
+#include <gtest/gtest.h>
+
+struct ExampleOrdersTests: public ::testing::Test
+{
+    Book* book;
+    OrderPipeline* orderPipeline;
+    GenerateOrders* generateOrders;
+
+    virtual void SetUp() override{
+        book = new Book();
+        orderPipeline = new OrderPipeline(book);
+        generateOrders = new GenerateOrders(book);
+    }
+
+    virtual void TearDown() override{
+        delete generateOrders;
+        delete orderPipeline;
+        delete book;
+    }
+};
+
+TEST_F(ExampleOrdersTests, CreateInitialOrdersTest) {
+    generateOrders->createInitialOrders(10000, 300);
+}
+
+TEST_F(ExampleOrdersTests, ProcessInitialOrdersTest) {
+    orderPipeline->processOrdersFromFile("C:/Users/benja/Documents/Limit-Order-Book/Limit_order_book/initialOrders.txt");
+}
+
+TEST_F(ExampleOrdersTests, CreateOrdersTest) {
+    orderPipeline->processOrdersFromFile("C:/Users/benja/Documents/Limit-Order-Book/Limit_order_book/initialOrders.txt");
+    generateOrders->createOrders(100000);
+}
